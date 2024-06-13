a6f0b571db17e746c9c2cf8bc828d9bc6f38d107
Lei da sorte <75717568+Frederick528@users.noreply.github.com>
2024-06-13 22:25:58 +0900

    Update CircleStartState.cs
    Remove Comment

--- Assets/Scripts/State/States/CircleStartState.cs
+++ Assets/Scripts/State/States/CircleStartState.cs
@@ -8,8 +8,6 @@ public class CircleStartState : MonoBehaviour, ICircleState
 
     public void Handle(CircleCtrl controller)
     {
-        // Actually, It don't need parameter because it's not using parameter right now.
-        // Just use EventBus...
         if (!_controller)
             _controller = controller;
 
